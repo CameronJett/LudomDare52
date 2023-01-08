99280c896c7879fe92716c539d7445dec903f14f
markvincentparsons <markparsons614@gmail.com>
2023-01-08 12:56:01 -0500

    Windows change color when player enters them, allowing interactivity

---
+++ nightmare_harvest/Assets/Scripts/Interactable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            spriteRenderer.color = new Color32 (50, 181, 233, 255);
+        }
+    }
+}
