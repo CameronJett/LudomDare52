cd9fdd5d7e961ab209f9c838016750a76f3dd4f1
Cameron Jett <cameron.jett18@gmail.com>
2023-01-08 01:15:55 -0500

    Game over Ui

---
+++ nightmare_harvest/Assets/Scripts/GameOverUi.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameOverUi : MonoBehaviour {
+
+	private void Awake()
+	{
+		VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+		root.Q<Button>("RestartButton").clicked += () => Debug.Log("RESTART CLICKED TODO");
+		root.Q<Button>("QuitButton").clicked += () => QuitGame();
+	}
+
+	private void QuitGame() {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+			Application.Quit();
+		}
+}
--- nightmare_harvest/Assets/Scripts/NightmareHarvestUi.cs
+++ nightmare_harvest/Assets/Scripts/NightmareHarvestUi.cs
@@ -3,10 +3,13 @@ using UnityEngine.UIElements;
 
 public class NightmareHarvestUi : MonoBehaviour {
 
-    private float time = 300;
+    private float time = 180;
     private int score = 0;
     private void Update()
     {
+        VisualElement game_over_ui = GameObject.Find("Game Over Ui").GetComponent<UIDocument>().rootVisualElement;
+        game_over_ui.visible = false;
+
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
         Label timerLabel = root.Q<Label>("TimerLabel");
@@ -15,8 +18,11 @@ public class NightmareHarvestUi : MonoBehaviour {
         timerLabel.text = this.getTime();
 
         if (time <= 0) {
-            // TODO: end game
             timerLabel.text = "Time: 0:00";
+            root.visible = false;
+
+            game_over_ui.Q<Label>("ScoreLabel").text = "Score: " + score.ToString();
+            game_over_ui.visible = true;
         }
 
         scoreLabel.text = "Score: " + score.ToString();
