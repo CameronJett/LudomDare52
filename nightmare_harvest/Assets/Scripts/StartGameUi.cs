d7dce782ba75b0e3e6de6613882ec105d83108a1
Cameron Jett <cameron.jett18@gmail.com>
2023-01-08 19:21:53 -0500

    Updated main scene

--- nightmare_harvest/Assets/Scripts/StartGameUi.cs
+++ nightmare_harvest/Assets/Scripts/StartGameUi.cs
@@ -15,21 +15,21 @@ public class StartGameUi : MonoBehaviour
 	public void StartGame()
 	{
 		Player player = GameObject.Find("Player").GetComponent<Player>();
-		Camera camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+		Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
 		Inventory inventory = GameObject.Find("Player").GetComponent<Inventory>();
 		VisualElement main_ui = GameObject.Find("Ui").GetComponent<UIDocument>().rootVisualElement;
 		NightmareHarvestUi harvest_ui = GameObject.Find("Ui").GetComponent<NightmareHarvestUi>();
 
 
 		harvest_ui.score = 0;
-		harvest_ui.time = 10;
+		harvest_ui.time = 180;
 
 		inventory.green_seed = 5;
 		inventory.blue_seed = 3;
 		inventory.red_seed = 3;
 
-		player.transform.SetPositionAndRotation(new Vector3(0, (float)-2.352596, 0), player.transform.rotation);
-		camera.transform.SetPositionAndRotation(new Vector3(0, (float)-2.352596, camera.transform.position.z), player.transform.rotation);
+		player.transform.SetPositionAndRotation(new Vector3((float)-13.68, (float)-4.07, 0), player.transform.rotation);
+		camera.transform.SetPositionAndRotation(new Vector3((float)-13.68, (float)-1.9, camera.transform.position.z), player.transform.rotation);
 
 		// Reset all starting variables here
 
