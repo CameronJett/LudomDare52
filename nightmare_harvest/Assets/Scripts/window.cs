e950c6b37a7483bdd2efe39792a3091e95460103
Cameron Jett <cameron.jett18@gmail.com>
2023-01-08 21:48:51 -0500

    adding plant stuff without main

--- nightmare_harvest/Assets/Scripts/window.cs
+++ nightmare_harvest/Assets/Scripts/window.cs
@@ -8,28 +8,29 @@ public class window : MonoBehaviour
     bool isAboutToWakeUp = false;
     int count = 0;
     GameObject sleepPlaceholder;
+    Transform sleepBubble; 
 
     // Start is called before the first frame update
     void Start()
     {
-
-        // Debug.Log(gameObject.transform.Find("SleepPlaceholder"));
-        Transform t1 = transform.GetChild(0).GetChild(0);
-        t1.GetComponent<Renderer>().enabled = false;
-        // sleepPlaceholder = gameObject.transform.Find("SleepPlaceholder").GetComponent<GameObject>();
-        // sleepPlaceholder.GetComponent<Renderer>().enabled = false;
-
+        sleepBubble = transform.GetChild(1);
+        WakeUp();
     }
 
     // Update is called once per frame
     void Update()
     {
         {
-            if (count >= 60 * 30) // only update every 30 seconds
+            if (count >= 30 * 10) // only update every 10 seconds
                 {
                     RunSleepCycle();
                     count = 0;
                 }
+
+            if (count % 30 == 0)
+            {
+                FlickerIfWakingUp();
+            }
             count++;
         }
     }
@@ -53,8 +54,15 @@ public class window : MonoBehaviour
         if (StrB >= 18)
         {
             Debug.Log("going to sleep: " + StrB);
-            isSleeping = true;
+            GoToSleep();
+            
         }
+    }
+
+    void GoToSleep()
+    {
+        isSleeping = true;
+        sleepBubble.GetComponent<Renderer>().enabled = true;
     }
 
     void TryToWakeUp()
@@ -76,10 +84,18 @@ public class window : MonoBehaviour
     {
         isSleeping = false;
         isAboutToWakeUp = false;
+        sleepBubble.GetComponent<Renderer>().enabled = false;
     }
 
     void HitSnoozeButton()
     {
         isAboutToWakeUp = true;
+    }
+
+    void FlickerIfWakingUp() {
+        if (isAboutToWakeUp)
+        {
+            sleepBubble.GetComponent<Renderer>().enabled = !sleepBubble.GetComponent<Renderer>().enabled;
+        }
     }
 }
